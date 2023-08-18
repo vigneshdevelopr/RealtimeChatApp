9345f75deb6aa1cd07413c3e803c1f37e6b1e997
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-08-18 22:51:37 +0530

    Initial Commit,Add Project to Github Repository

---
+++ src/FireBase/FireBaseConfig.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDM6nrJN053I-scCdB2s1Ci1Rex_EXGbZs",
+    authDomain: "realchat-app-7c030.firebaseapp.com",
+    projectId: "realchat-app-7c030",
+    storageBucket: "realchat-app-7c030.appspot.com",
+    messagingSenderId: "795693258131",
+    appId: "1:795693258131:web:92becc721e4a684b6b0466",
+    measurementId: "G-JDPEN537VM"
+  };
+
+  export const fireBaseConfig = firebaseConfig;
