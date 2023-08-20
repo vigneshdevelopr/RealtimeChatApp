1b1f2baba3a0ae877d192b5fd9d4d3473b869c52
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-08-20 17:59:25 +0530

    Initial

--- src/FireBase/FireBaseConfig.jsx
+++ src/FireBase/FireBaseConfig.jsx
@@ -1,11 +1,18 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDM6nrJN053I-scCdB2s1Ci1Rex_EXGbZs",
-    authDomain: "realchat-app-7c030.firebaseapp.com",
-    projectId: "realchat-app-7c030",
-    storageBucket: "realchat-app-7c030.appspot.com",
-    messagingSenderId: "795693258131",
-    appId: "1:795693258131:web:92becc721e4a684b6b0466",
-    measurementId: "G-JDPEN537VM"
-  };
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-  export const fireBaseConfig = firebaseConfig;
+const firebaseConfig = {
+  apiKey: "AIzaSyDM6nrJN053I-scCdB2s1Ci1Rex_EXGbZs",
+  authDomain: "realchat-app-7c030.firebaseapp.com",
+  projectId: "realchat-app-7c030",
+  storageBucket: "realchat-app-7c030.appspot.com",
+  messagingSenderId: "795693258131",
+  appId: "1:795693258131:web:92becc721e4a684b6b0466",
+  measurementId: "G-JDPEN537VM",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export { app, auth, provider };
